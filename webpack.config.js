a89a3626589b3fa721c9340a9e7be3223849b447
SpidySamurai <javierchiortiz@gmail.com>
2021-08-20 21:13:17 -0500

    File compressor

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,15 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"), //Current directory-local
-    filename: "main.js",
+    filename: "[name].[contenthash].js",
+    assetModuleFilename: "assets/images/[hash][ext][query]",
   },
   resolve: {
     extensions: [".js"],
@@ -30,6 +33,30 @@ module.exports = {
         test: /\.png/,
         type: "asset/resource",
       },
+      {
+        test: /\.woff|woff2$/,
+        use: {
+          loader: "url-loader",
+          options: {
+            limit: 10000, // O LE PASAMOS UN BOOLEANOS TRUE O FALSE
+            // Habilita o deshabilita la transformación de archivos en base64.
+            mimetype: "aplication/font-woff",
+            // Especifica el tipo MIME con el que se alineará el archivo.
+            // Los MIME Types (Multipurpose Internet Mail Extensions)
+            // son la manera standard de mandar contenido a través de la red.
+            name: "[name].[contenthash].[ext]",
+            // EL NOMBRE INICIAL DEL ARCHIVO + SU EXTENSIÓN
+            // PUEDES AGREGARLE [name]hola.[ext] y el output del archivo seria
+            // ubuntu-regularhola.woff
+            outputPath: "./assets/fonts/",
+            // EL DIRECTORIO DE SALIDA (SIN COMPLICACIONES)
+            publicPath: "./assets/fonts/",
+            // EL DIRECTORIO PUBLICO (SIN COMPLICACIONES)
+            esModule: false,
+            // AVISAR EXPLICITAMENTE SI ES UN MODULO
+          },
+        },
+      },
     ],
   },
   plugins: [
@@ -38,7 +65,9 @@ module.exports = {
       template: "./public/index.html",
       filename: "./index.html",
     }),
-    new MiniCssExtractPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "assets/[name].[contenthash].css",
+    }),
     new CopyPlugin({
       patterns: [
         {
@@ -48,4 +77,8 @@ module.exports = {
       ],
     }),
   ],
+  optimization: {
+    minimize: true,
+    minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
+  },
 };
